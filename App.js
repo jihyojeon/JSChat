4d073208c8b958a119aba6e3dcd2d1b64c009364
JIHYO-JEON <cori950920@gmail.com>
2021-07-02 15:11:45 +0900

    Add time

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
 let messages = document.querySelector('.message-list');
 let btn = document.querySelector('.btn');
 let input = document.querySelector('input');
+// let dateToday = new Date.toLocaleDateString();
+// let dateShown = ''
 
 // Click 'Send' button or press Enter Key
 btn.addEventListener('click', sendMessage);
@@ -12,20 +14,40 @@ function sendMessage(){
     let msg = input.value;
     if (msg !== ''){
         input.value = '';
+        // showDate();
         writeLine(msg);
         translate(msg);
     }
 }
 
+// function showDate(){
+//     if (dateShown !== dateToday){
+//         let d = document.createElement('li');
+//         d.classList.add('message-item', 'date');
+//         d.innerHTML = dateToday;
+//         dateShown = dateToday;
+//         messages.appendChild(d);
+//         messages.scrollTop = messages.scrollHeight;
+//     }
+// }
+
 function writeLine(text){
+    // Show Messege
     let message = document.createElement('li');
     message.classList.add('message-item', 'item-user');
     message.innerHTML = text;
     messages.appendChild(message);
     messages.scrollTop = messages.scrollHeight;
+    // Show time
+    let time = document.createElement('li');
+    time.classList.add('message-item', 'time-user');
+    time.innerHTML = new Date().toLocaleTimeString();
+    messages.appendChild(time);
+    messages.scrollTop = messages.scrollHeight;
 }
 
 function translate(text){
+    // Show message
     let message = document.createElement('li');
     message.classList.add('message-item', 'item-trans');
     google.language.translate(text, "en", "ko", function(result)
@@ -35,5 +57,11 @@ function translate(text){
         }
     });
     messages.appendChild(message);
+    messages.scrollTop = messages.scrollHeight;
+    // show time
+    let time = document.createElement('li');
+    time.classList.add('message-item', 'time-trans');
+    time.innerHTML = new Date().toLocaleTimeString();
+    messages.appendChild(time);
     messages.scrollTop = messages.scrollHeight;
  }
